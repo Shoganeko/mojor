43e65370056caedfef012390b6a76cdeeddb12d4
sho <blaziyt@gmail.com>
2019-11-14 21:27:45 -0600

    Updated health page

--- src/main/java/dev/shog/mojor/api/Health.kt
+++ src/main/java/dev/shog/mojor/api/Health.kt
@@ -1,5 +1,6 @@
 package dev.shog.mojor.api
 
+import dev.shog.mojor.db.PostgreSql
 import reactor.core.publisher.Flux
 
 /**
@@ -9,12 +10,26 @@ object Health {
     /**
      * Get the health of the current services.I
      */
-    fun getCurrentHealth(): Flux<Pair<String, String>> =
-            Flux.just(getServerHealth())
+    fun getCurrentHealth(): Flux<Pair<String, Any>> =
+            Flux.just(getServerHealth(), getDatabaseHealth())
 
     /**
      * The server is online.
      */
-    private fun getServerHealth(): Pair<String, String> =
-            Pair("server", "OK")
+    private fun getServerHealth(): Pair<String, Any> =
+            Pair("servers", object {
+                val api = "OK"
+                val cdn = "OK"
+                val main = "OK"
+            })
+
+    /**
+     * The database is online
+     */
+    private fun getDatabaseHealth(): Pair<String, Any> {
+        PostgreSql.createConnection()
+                ?: return Pair("database", "NOT OK")
+
+        return Pair("database", "OK")
+    }
 }
