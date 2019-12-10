9d76d89cbcbb8ef2b7f030ae63a4b7bc1903359b
SHO <36061227+Shoganeko@users.noreply.github.com>
2019-12-10 08:06:24 -0600

    Client-side encryption

--- src/main/resources/js/motd.js
+++ src/main/resources/js/motd.js
@@ -6,14 +6,14 @@ $( document ).ready(function () {
 
     $("#submit").click(function () {
         const username = $("#username").val();
-        const password = $("#password").val();
+        const password = sha512($("#password").val());
         const text = $("#text").val();
 
         if (!prevAuth) {
             $("#result").text("Authorizing...");
 
             // TODO encrypt client-side
-            $.post("https://api.shog.dev/v1/user", { username: username, password: password, encr: true }, function (data) {
+            $.post("https://api.shog.dev/v1/user", { username: username, password: password }, function (data) {
                 const token = data.token.token;
 
                 $.ajaxSetup({headers: {'Authorization': "token " + token}});
@@ -25,6 +25,13 @@ $( document ).ready(function () {
         } else updateMotd(text, owner);
     });
 });
+
+// Honestly dont know how this works, but it does :)
+function sha512(str) {
+    return crypto.subtle.digest("SHA-512", new TextEncoder("utf-8").encode(str)).then(buf => {
+        return Array.prototype.map.call(new Uint8Array(buf), x=>(('00'+x.toString(16)).slice(-2))).join('');
+    });
+}
 
 function updateMotd(text, owner) {
     $.post("https://api.shog.dev/motd", { text: text, owner: owner }, function () {
