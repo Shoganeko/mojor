9ff55afdc48ff4a940325606ef0b63a097bdbb75
sho <blaziyt@gmail.com>
2019-12-26 13:58:13 -0600

    Added a permissions overview to account page

--- src/main/java/dev/shog/mojor/pages/Account.kt
+++ src/main/java/dev/shog/mojor/pages/Account.kt
@@ -1,7 +1,10 @@
 package dev.shog.mojor.pages
 
+import dev.shog.mojor.Mojor
+import dev.shog.mojor.auth.obj.Permissions
 import dev.shog.mojor.auth.obj.Session
 import dev.shog.mojor.auth.token.TokenHolder
+import dev.shog.mojor.auth.user.User
 import dev.shog.mojor.auth.user.UserHolder
 import dev.shog.mojor.getSession
 import dev.shog.mojor.handle.markdown.MarkdownPage
@@ -19,6 +22,18 @@ object Account : RegPage {
 
         return SignedOut.getPage(call)
     }
+
+    fun getPermissions(user: User): String =
+            buildString {
+                user.permissions.forEach { permissions ->
+                    when (permissions) {
+                        Permissions.APP_MANAGER -> append("You have permissions to manage Mojor. <br/>")
+                        Permissions.BUTA_MANAGER -> append("You have permission to manage Buta. <br/>")
+                        Permissions.USER_MANAGER -> append("You have permission to manage Users. <br/>")
+                        Permissions.MOTD_MANAGER -> append("You have permission to manage MOTDS. You can do this [here](${Mojor.MAIN}).")
+                    }
+                }
+            }
 
     private val ELEVATED_SIGN_IN = MarkdownPage("account/elevated-account.md").respond()
     private val SIGNED_IN = MarkdownPage("account/signed-in.md").respond()
@@ -53,6 +68,7 @@ object Account : RegPage {
                         .replace("{name}", owner.username)
                         .replace("{sign-in-date}", Homepage.formatter.format(Date.from(Instant.ofEpochMilli(ses.signInDate))))
                         .replace("{sign-in-ip}", ses.signInIp)
+                        .replace("{permissions}", getPermissions(owner))
             }
         }
     }
