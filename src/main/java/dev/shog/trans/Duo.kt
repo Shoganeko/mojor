502e5b178037b0df670a03ab3eb455b3fecb55c0
sho <blaziyt@gmail.com>
2020-01-02 08:50:47 -0600

    Added docs to Duo

--- src/main/java/dev/shog/trans/Duo.kt
+++ src/main/java/dev/shog/trans/Duo.kt
@@ -1,6 +1,12 @@
 package dev.shog.trans
 
+/**
+ * A pair, but it's able to be used through Jackson for transport through Mojor.
+ */
 class Duo<T, R>(val first: T? = null, val second: R? = null)
 
+/**
+ * Similar to function [to].
+ */
 infix fun <T, R> T.duo(r: R): Duo<T, R> =
         Duo(this, r)
